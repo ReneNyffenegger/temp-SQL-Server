b0ef106cc937fc883b3faa0d103bda5c413560f7
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-09-20 15:31:08 +0200

    Update cleanup.sql

--- job/cleanup.sql
+++ job/cleanup.sql
@@ -5,9 +5,12 @@ exec msdb..sp_delete_jobserver
 
 
 
-
 exec msdb..sp_delete_jobstep 
    @step_id  =  1,
    @job_name = 'tq84_job_example_job';
 
 exec msdb..sp_delete_job @job_name = N'tq84_job_example_job';
+   
+exec msdb..sp_delete_schedule @schedule_name = N'tq84_job_example_schedule'   
+
+
