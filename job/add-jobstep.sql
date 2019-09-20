0ec9c0af83af2d2315e975ca8455a9ca5d72de5e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-09-20 15:02:37 +0200

    Update add-jobstep.sql

--- job/add-jobstep.sql
+++ job/add-jobstep.sql
@@ -1,7 +1,10 @@
 exec msdb..sp_add_jobstep  
-    @job_name       = N'tq84_job_example_job',  
-    @step_name      = N'tq84_job_example_step',
-    @subsystem      = N'TSQL',  
-    @command        = N'exec tq84_job_example_proc',   
-    @retry_attempts = 0,  
-    @retry_interval = 0;  
+    @job_name          = N'tq84_job_example_job',  
+    @step_name         = N'tq84_job_example_step',
+    @step_id           =   1,  -- Used to order steps.
+    @subsystem         = N'TSQL',  
+    @command           = N'exec tq84_job_example_proc',
+    @on_success_action =   3, -- Go to next step
+    @retry_attempts    =   0,  
+    @retry_interval    =   0;  
+;
